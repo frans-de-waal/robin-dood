e6f41f51c6177368438a02615e47b253b6889154
Frans de Waal <fmdewaal@gmail.com>
2023-04-02 20:02:07 +0200

    Updated the base config for GitHub Pages deployment

--- src/game.js
+++ src/game.js
@@ -201,6 +201,7 @@ export function setup() {
     player.draw(scene)
     // scene.drawScale()
   }
+
   scene.canvas.addEventListener('mousedown', (event) => {
     const canvasRect = scene.canvas.getBoundingClientRect()
     const mousePos = new Position(
